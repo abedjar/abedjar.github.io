2dc421e9658d2e07c880d7d7f644a03bef751dfb
abedjar <abedjar@gmail.com>
2024-03-11 01:26:17 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-const Counter = {
+/*const Counter = {
     data() {
         return resume
     }
 }
 
-Vue.createApp(Counter).mount('#app')
+Vue.createApp(Counter).mount('#app')*/
+
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      resume
+    };
+  }
+});
